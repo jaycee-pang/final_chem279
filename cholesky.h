b2967c4350587504c21e56728d41c36606090c71
Jaycee Pang <jayceepang@berkeley.edu>
2024-04-28 19:46:00 -0700

    update

--- cholesky.h
+++ cholesky.h
@@ -1,12 +1,29 @@
 #include <iostream>
 #include <cmath> 
-#include <arma> 
+#include <armadillo> 
+/*
+.is_trimatu / .is_trimatl	 	check whether matrix is upper/lower triangular
+.is_diagmat	 	check whether matrix is diagonal
+.is_square	 	check whether matrix is square sized
+.is_symmetric	 	check whether matrix is symmetric
+.is_hermitian	 	check whether matrix is hermitian
+.is_sympd	 	check whether matrix is symmetric/hermitian positive definite
+*/
 
-int find_pivot(const mat& A, int start) {
-    int n = A.n_rows();
+/**
+ * Find the pivot in the column A from the given starting index. 
+ *
+ * Look down a column to find the maximum value. 
+ *
+ * @param A: matrix A (matrix to be decomposed)
+ * @return pivot row: row index of the maximum value in the start column to be used as the pivot. 
+ */
+int find_pivot(const arma::mat& A, int start) {
+    int n = A.n_rows;
     int pivot_row = start; 
     double max_val = std::abs(A(start, start)); 
     // find the max val 
+    // compute elements below the idagonal 
     for (int i=start+1; i<n ; i++) {
         if (std::abs(A(i,start)) > max_val) {
             max_val = std::abs(A(i, start));
@@ -28,77 +45,43 @@ pivoted:
 
 L is lower triangular
 */
-bool cholesky(const arma::mat & A, arma::mat& L, bool pivot) {
-    int n = A.n_rows();
+
+
+bool pivoted_cholesky(arma::mat& A, arma::mat& L, bool pivot) {
+    int n = A.n_rows;
     L.zeros(); 
     arma::uvec p(n); 
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<=i; j++) {
-            double sum=0.0; 
-            for (int k=0; k<j; k++) {
-                sum += L(i, k)*L(j,k);
+    p.zeros();
 
-                if (i==j) {
-                    // check for positive definiteness 
-                    L(i, j) = std::sqrt(A(i,i) - sum);
-                }
-                else {
-                    if (L(j,j) == 0) {return false;}
-                    L(i, j) = (1.0 /L(j, j))*(A(i, j) - sum);
-                }
+    for (int i = 0; i < n; i++) {
+        if (pivot) {
+            int pivot_row = find_pivot(A, i); 
+            if (pivot_row != i) {
+                // use arma::swap rows 
+                A.swap_rows(i,pivot_row);
+                L.swap_rows(i,pivot_row);
+                p(i) = pivot_row;
+            }
+
+        }
+        else {
+            p(i) = i;
+        }
+        
+        for (int j = i; j<n; j++) {
+            double sum = 0.0; 
+            for (int k = 0; k<i; k++) {
+                sum += L(j,k)*L(i, k);
+            }
+            if (i == j) { 
+                // check for positive definiteness 
+                if (A(i,i) - sum <= 0) {return false; }
+                L(i,j) = std::sqrt(A(i,i)-sum);
+            } else {
+                if (L(j,j) == 0) {return false; } // Zero division
+                L(j,i) = (1.0 /L(j,j)) * (A(j,i) - sum);
             }
         }
     }
     return true;
-
-}
-
-
-bool pivoted_cholesky(const mat& A, mat& L) {
-    int n = A.n_rows();
-    int n = A.n_rows();
-    L.zeros(); 
-    arma::uvec p(n);
-
-    for (int i=0; i<n; i++) {
-        int pivot_row = find_pivot(A,i); 
-        if (pivot_row != i) {
-            // swap rows i, pivot row of A 
-            // swap i, pivot rows of L 
-            p(i) = pivot_row;
-
-        }
-
-        for (int j=i; j<n; j++) {
-            double sum=0.0; 
-            for (int k=0; k<j; k++) {
-                sum += L(j,k)*L(j, k);
-
-                if (i==j) {
-                    // check for positive definiteness 
-                    L(i, j) = std::sqrt(A(i,j) - sum);
-                }
-                else {
-                    if (L(j,j) == 0) {return false;}
-                    L(j,i) = (1.0 / L(j,j)) * (A(j,i) - sum);
-
-                }
-            
-                
-                // double diagonal = L(j,j); 
-                // for (int k=i+1; k<n; k++) {
-                //     L(k,i) /= digonal;
-                // }
-                
-                // L(i, j) = (1.0 /L(j, j))*(A(i, j) - sum);
-            
-            }
-        }
-
-    return true;
-    }
-
-
-        
-        
 }
